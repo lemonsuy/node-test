79b0e922963ca3e4a6f34fe577294c5fc3ed42e9
lemonsuy <pattharathon.srithundorn@mail.kmutt.ac.th>
2020-06-13 23:31:46 +0800

    add package.json file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => res.send('Hello Mr.Lemon!'))
 
-app.listen(port, () => console.log(`Example app listening at http://161.117.59.225:3000`))
+app.listen(port, () => console.log(`Example app listening at http://161.117.59.225:5000`))
